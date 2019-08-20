25cc8e02540b8c4fb4ab6c50b9fa677f5806dbb4
Jigal Sanders <j.sanders@hypotheekbond.nl>
2019-08-20 21:38:41 +0200

    [master] HF

--- app/Nik/NikCrawlerObserver.php
+++ app/Nik/NikCrawlerObserver.php
@@ -82,6 +82,7 @@ final class NikCrawlerObserver extends CrawlObserver
                 ['brand_id', '=', $brand->id]
             ])->firstOrFail();
         } catch (ModelNotFoundException $e) {
+            dd($brand);
             $product = new Product();
             $product->brand_id = $brand->id;
             $product->productName = ucfirst(trim($data[3]));
