e7dfb3985dbe0c6b3a0222aa74d6de1a86b1b348
Jigal Sanders <j.sanders@hypotheekbond.nl>
2019-09-03 09:36:22 +0200

    WIP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,51 +1,55 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139719882-2"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
 
-          gtag('config', 'UA-139719882-2');
-        </script>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Een database met koosjere producten in Nederland">
-        <meta name="keywords" content="koosjer, is het koosjer, ishetkoosjer, kosher, is het kosher">
-        <meta property="og:title" content="Is het Koosjer">
-        <meta property="og:description" content="Een database met koosjere producten in Nederland">
-        <meta property="og:type" content="website">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Is het Koosjer?</title>
-        <link rel="stylesheet" href="/css/app.css">
-    </head>
-    <body>
-      <div id="app" class="antialiased text-gray-900">
-          <div>
-              <div class="flex">
-{{--                  <aside class="w-1/4 bg-gray-800 pt-8 min-h-screen">--}}
-{{--                      <header class="p-6 mb-8">--}}
-{{--                          <h1>NIK Kasjroetlijst</h1>--}}
-{{--                      </header>--}}
-{{--                      <section class="mb-10 pl-4">--}}
-{{--                          <h5 class="text-gray-600 uppercase font-bold mb-4 text-base">Navigation</h5>--}}
-{{--                          <ul class="text-gray-400 ">--}}
-{{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  to="/" exact>Home</router-link></li>--}}
-{{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'products'}">Producten</router-link></li>--}}
-{{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'ingredients'}">Ingredi&euml;nten</router-link></li>--}}
-{{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'categories'}">Categorie&euml;n</router-link></li>--}}
-{{--                              <li class="text-sm hover:bg-gray-700  leading-large"><router-link  :to="{name: 'brands'}">Merken</router-link></li>--}}
-{{--                          </ul>--}}
-{{--                      </section>--}}
-{{--                  </aside>--}}
-                  <main class="primary w-3/4 flex-1 px-6 bg-gray-100">
-                      <router-view></router-view>
-                  </main>
-              </div>
-          </div>
-      </div>
-      <script src="/js/app.js"></script>
-    </body>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139719882-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-139719882-2');
+    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Een database met koosjere producten in Nederland">
+    <meta name="keywords" content="koosjer, is het koosjer, ishetkoosjer, kosher, is het kosher">
+    <meta property="og:title" content="Is het Koosjer">
+    <meta property="og:description" content="Een database met koosjere producten in Nederland">
+    <meta property="og:type" content="website">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Is het Koosjer?</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="manifest" href="manifest.webmanifest">
+</head>
+
+<body>
+    <div id="app" class="antialiased text-gray-900">
+        <div>
+            <div class="flex">
+                {{--                  <aside class="w-1/4 bg-gray-800 pt-8 min-h-screen">--}}
+                {{--                      <header class="p-6 mb-8">--}}
+                {{--                          <h1>NIK Kasjroetlijst</h1>--}}
+                {{--                      </header>--}}
+                {{--                      <section class="mb-10 pl-4">--}}
+                {{--                          <h5 class="text-gray-600 uppercase font-bold mb-4 text-base">Navigation</h5>--}}
+                {{--                          <ul class="text-gray-400 ">--}}
+                {{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  to="/" exact>Home</router-link></li>--}}
+                {{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'products'}">Producten</router-link></li>--}}
+                {{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'ingredients'}">Ingredi&euml;nten</router-link></li>--}}
+                {{--                              <li class="text-sm hover:bg-gray-700 leading-large"><router-link  :to="{name: 'categories'}">Categorie&euml;n</router-link></li>--}}
+                {{--                              <li class="text-sm hover:bg-gray-700  leading-large"><router-link  :to="{name: 'brands'}">Merken</router-link></li>--}}
+                {{--                          </ul>--}}
+                {{--                      </section>--}}
+                {{--                  </aside>--}}
+                <main class="primary w-3/4 flex-1 px-6 bg-gray-100">
+                    <router-view></router-view>
+                </main>
+            </div>
+        </div>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+
 </html>
